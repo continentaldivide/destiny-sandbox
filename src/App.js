f8c2c04269653113cdeb27c64663e56743dcdec7
Andrew <andrew@andrewsmith.org>
2022-12-06 23:13:55 -0500

    Implement connectivity with Bungie API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 const App = () => {
+  const apiKey = process.env.REACT_APP_DESTINY_API_KEY;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/",
+    true
+  );
+  xhr.setRequestHeader("X-API-Key", apiKey);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var json = JSON.parse(this.responseText);
+      console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+    }
+  };
+
+  xhr.send();
+
   return (
     <div>
       <p>hello world</p>
